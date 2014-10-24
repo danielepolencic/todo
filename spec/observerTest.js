3cd75cf2aa2410e7096c301d3a45d2f711e37e86
Daniele Polencic <daniele.polencic@gmail.com>
2014-10-24 23:00:21 +0100

    improved observer harness

--- spec/observerTest.js
+++ spec/observerTest.js
@@ -1,33 +1,98 @@
-(function(App){
+(function (TodoApp) {
 
   'use strict';
 
-  describe( 'Observer', function(){
+  describe('Observer', function () {
 
     var observer;
 
     beforeEach(function() {
-      observer = new App.Observer();
+      observer = new TodoApp.Observer();
     });
 
-    it( 'should subscribe to an event', function(){
+    describe('Observer.prototype.constructor', function () {});
 
-      observer.subscribe('test', function(){
-        var args = Array.prototype.slice.call( arguments );
-        expect(args[0]).toEqual('test-string');
+    describe('Observer.prototype.subscribe', function () {
+
+      it('should subscribe to topic', function () {
+        var listener = jasmine.createSpy('listener');
+        expect(observer.subscribe('test', listener)).toBe(true);
       });
 
-      observer.publish('test', 'test-string');
+      it('should not subscribe if the event is invalid', function () {
+        var listener = jasmine.createSpy('listener');
+        expect(observer.subscribe(null, listener)).toBe(false);
+        expect(observer.subscribe([], listener)).toBe(false);
+        expect(observer.subscribe('', listener)).toBe(false);
+        expect(observer.subscribe(' ', listener)).toBe(false);
+      });
+
+      it('should not subscribe if the callback is not a function', function () {
+        expect(observer.subscribe('test', [])).toBe(false);
+        expect(observer.subscribe('test', 'a')).toBe(false);
+      });
+
     });
 
-    it( 'should unsubscribe from event', function(){
-      var callback = jasmine.createSpy('callback');
-      observer.subscribe('unsub', callback);
-      observer.unsubscribe('unsub', callback);
-      observer.publish('unsub', 'none should reply');
-      expect(callback).not.toHaveBeenCalled();
+    describe('Observer.prototype.unsubscribe', function () {
+
+      it('should unsubscribe from a topic', function () {
+        var listener = jasmine.createSpy('listener');
+        observer.subscribe('unsub', listener);
+        expect(observer.unsubscribe('unsub', listener)).toBe(true);
+      });
+
+      it('should not unsubscribe if the topic is new', function () {
+        expect(observer.unsubscribe('unsub', function () {})).toBe(false);
+      });
+
+      it('should not unsubscribe if the event is invalid', function () {
+        var listener = jasmine.createSpy('listener');
+        expect(observer.unsubscribe(null, listener)).toBe(false);
+        expect(observer.unsubscribe([], listener)).toBe(false);
+        expect(observer.unsubscribe('', listener)).toBe(false);
+        expect(observer.unsubscribe(' ', listener)).toBe(false);
+      });
+
+      it('should not unsubscribe if the callback is not a function', function () {
+        expect(observer.unsubscribe('test', [])).toBe(false);
+        expect(observer.unsubscribe('test', 'a')).toBe(false);
+      });
+
+    });
+
+    describe('Observer.prototype.publish', function () {
+
+      it('should publish to a topic', function () {
+        var listenerA = jasmine.createSpy('listenerA');
+        var listenerB = jasmine.createSpy('listenerB');
+        observer.subscribe('topicA', listenerA);
+        observer.subscribe('topicB', listenerB);
+
+        expect(observer.publish('topicA', 'test-string')).toBe(true);
+        expect(listenerA).toHaveBeenCalledWith('test-string');
+        expect(listenerB).not.toHaveBeenCalled();
+      });
+
+      it('should not publish an event if the topic is invalid', function () {
+        expect(observer.publish()).toBe(false);
+        expect(observer.publish('')).toBe(false);
+        expect(observer.publish(' ')).toBe(false);
+      });
+
+      it('should publish to a topic multiple arguments', function () {
+        var listenerA = jasmine.createSpy('listener');
+        var listenerB = jasmine.createSpy('listener');
+        observer.subscribe('test', listenerA);
+        observer.subscribe('test', listenerB);
+
+        expect(observer.publish('test', 'test-string', 1, ['a'])).toBe(true);
+        expect(listenerA).toHaveBeenCalledWith('test-string', 1, ['a']);
+        expect(listenerB).toHaveBeenCalledWith('test-string', 1, ['a']);
+      });
+
     });
 
   });
 
-}).call(this, window.App || {});
+}).call(this, window.TodoApp || {});
--- src/observer.js
+++ src/observer.js
@@ -1,51 +1,61 @@
-(function(App){
+(function (TodoApp) {
 
   'use strict';
 
-  function Observer(){
-    this.cache = {};
+  function isString (anything) {
+    return Object.prototype.toString.call(anything) === '[object String]';
   }
 
-  Observer.prototype.subscribe = function( event, callback ){
-
-    if ( !event ) { return; }
-
-    if ( !Array.isArray(this.cache[event]) ) {
-      this.cache[event] = [];
-    }
-    this.cache[event].push( callback );
-
-  };
-
-  Observer.prototype.unsubscribe = function( event, callback ){
-
-    if( !this.cache[event] ) { return; }
-
-    this.cache[event] = this.cache[event].filter( function( cb ){
-      return cb !== callback;
-    });
-
-  };
-
-  Observer.prototype.publish = function( event ){
-
-    var args = Array.prototype.slice.call( arguments, 1 );
-    this.cache[event].forEach( function( subscriber ){
-      subscriber && subscriber.apply( null, args );
-    });
-
-  };
-
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = Observer;
-    }
-    exports.Observer = Observer;
-  } else {
-    this.Observer = Observer;
+  function isFunction (anything) {
+    return Object.prototype.toString.call(anything) === '[object Function]';
   }
 
-  App.Observer = Observer;
+  function Observer () {
+    this.listeners = new Map();
+  }
 
-}).call(this, window.App || {});
+  Observer.prototype.subscribe = function (event, callback) {
+    if (!isString(event) || event.trim() === '' || !isFunction(callback)) {
+      return false;
+    }
+
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
+    }
+
+    this.listeners.get(event).push(callback);
+    return true;
+  };
+
+  Observer.prototype.unsubscribe = function (event, callback) {
+    if (!isString(event) || event.trim() === '' || !isFunction(callback)) {
+      return false;
+    }
+
+    if (!this.listeners.has(event)) return false;
+
+    var subscribers = this.listeners.get(event).filter(function (subscriber) {
+      return callback !== subscriber;
+    });
+
+    this.listeners.set(event, subscribers);
+    return true;
+  };
+
+  Observer.prototype.publish = function (event){
+    if (!isString(event) || event.trim() === '' || !this.listeners.has(event)) {
+      return false;
+    }
+
+    var args = [].slice.call(arguments, 1);
+
+    this.listeners.get(event).forEach(function (subscriber) {
+      subscriber.apply(void 0, args);
+    });
+    return true;
+  };
+
+  TodoApp.Observer = Observer;
+
+}).call(this, window.TodoApp || {});
 
